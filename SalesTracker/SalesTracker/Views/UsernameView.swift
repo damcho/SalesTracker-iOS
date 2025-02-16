99ffb67c81ae0127b202197b71919b489d16c353
Damian Modernell <dmodernell@cohesionib.com>
2025-02-16 10:57:21 -0300

    username view initial implementation

--- SalesTracker/SalesTracker/Views/UsernameView.swift
+++ SalesTracker/SalesTracker/Views/UsernameView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct UsernameView: View {
+    @State private var username: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TextField(
+                "User name",
+                text: $username
+            )
+            .textInputAutocapitalization(.never)
+            .disableAutocorrection(true)
+            .border(.secondary)
+            .padding()
     }
 }
 
