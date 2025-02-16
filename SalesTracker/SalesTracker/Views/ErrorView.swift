31c03837cd458b75babfaa51becf9281e67a7d01
Damian Modernell <dmodernell@cohesionib.com>
2025-02-16 11:02:54 -0300

    Initial login errorview implementation

---
+++ SalesTracker/SalesTracker/Views/ErrorView.swift
@@ -0,0 +1,19 @@
+//
+//  ErrorView.swift
+//  SalesTracker
+//
+//  Created by Damian Modernell on 16/2/25.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text("Error view")
+            .foregroundStyle(.red)
+    }
+}
+
+#Preview {
+    ErrorView()
+}
