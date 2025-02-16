bf5f4eeff514db30a70de91a25804a56319a6ebc
Damian Modernell <dmodernell@cohesionib.com>
2025-02-16 11:23:28 -0300

    Implementing Login screen view

---
+++ SalesTracker/SalesTracker/Views/LoginScreen.swift
@@ -0,0 +1,36 @@
+//
+//  LoginScreen.swift
+//  SalesTracker
+//
+//  Created by Damian Modernell on 16/2/25.
+//
+
+import SwiftUI
+
+struct LoginScreen: View {
+    let errorView: ErrorView
+    let usernameView: UsernameView
+    let passwordView: PasswordView
+    let activityIndicatorView: ActivityIndicatorView
+    let loginButtonView: LoginButtonView
+    
+    var body: some View {
+        VStack {
+            activityIndicatorView
+            usernameView
+            passwordView
+            errorView
+            loginButtonView
+        }
+    }
+}
+
+#Preview {
+    LoginScreen(
+        errorView: ErrorView(),
+        usernameView: UsernameView(),
+        passwordView: PasswordView(),
+        activityIndicatorView: ActivityIndicatorView(),
+        loginButtonView: LoginButtonView()
+    )
+}
