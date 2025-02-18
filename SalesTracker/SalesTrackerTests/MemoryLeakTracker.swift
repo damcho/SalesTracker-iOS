1da9280917bc71564504f0fdc3711c65a4e26467
Damian Modernell <dmodernell@cohesionib.com>
2025-02-18 09:08:33 -0300

    Adding memory leak tracker struct

---
+++ SalesTracker/SalesTrackerTests/MemoryLeakTracker.swift
@@ -0,0 +1,18 @@
+//
+//  MemoryLeakTracker.swift
+//  SalesTrackerTests
+//
+//  Created by Damian Modernell on 18/2/25.
+//
+
+import Foundation
+import Testing
+
+struct MemoryLeakTracker<T: AnyObject> {
+    weak var instance: T?
+    var sourceLocation: SourceLocation
+    
+    func verify() {
+        #expect(instance == nil, "Expected \(instance) to be deallocated. Potential memory leak", sourceLocation: sourceLocation)
+    }
+}
