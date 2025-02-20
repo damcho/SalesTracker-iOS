c1aa9d780790f4b411cd8e9d100b424bbfb4e7b2
Damian Modernell <dmodernell@cohesionib.com>
2025-02-19 22:37:03 -0300

    Moving AuthenticationMapper to production

---
+++ SalesTracker/SalesTracker/Login/New Folder/AuthenticationMapper.swift
@@ -0,0 +1,41 @@
+//
+//  AuthenticationMapper.swift
+//  SalesTracker
+//
+//  Created by Damian Modernell on 19/2/25.
+//
+
+import Foundation
+
+struct DecodableHTTPErrorMessage: Decodable {
+    let message: String
+}
+
+struct DecodableAuthenticationResult: Decodable {
+    let access_token: String
+    
+    func toAuthenticationResult() -> AuthenticationResult {
+        .init(authToken: access_token)
+    }
+}
+
+enum AuthenticationMapper {
+    static let unauthorized = 401
+    static let success = 200
+    static func map(_ response: HTTPURLResponse, _ data: Data) throws -> AuthenticationResult {
+        switch response.statusCode {
+        case unauthorized:
+            let errorData = try JSONDecoder().decode(
+                DecodableHTTPErrorMessage.self,
+                from: data
+            )
+            throw LoginError.authentication(errorData.message)
+        case 400, 402..<499:
+            throw LoginError.connectivity
+        case success:
+            return try JSONDecoder().decode(DecodableAuthenticationResult.self, from: data).toAuthenticationResult()
+        default:
+            throw LoginError.other
+        }
+    }
+}
--- SalesTracker/SalesTrackerTests/Login/AuthenticationMapperTests.swift
+++ SalesTracker/SalesTrackerTests/Login/AuthenticationMapperTests.swift
@@ -10,39 +10,6 @@ import Foundation
 
 @testable import SalesTracker
 
-struct DecodableHTTPErrorMessage: Decodable {
-    let message: String
-}
-
-struct DecodableAuthenticationResult: Decodable {
-    let access_token: String
-    
-    func toAuthenticationResult() -> AuthenticationResult {
-        .init(authToken: access_token)
-    }
-}
-
-enum AuthenticationMapper {
-    static let unauthorized = 401
-    static let success = 200
-    static func map(_ response: HTTPURLResponse, _ data: Data) throws -> AuthenticationResult {
-        switch response.statusCode {
-        case unauthorized:
-            let errorData = try JSONDecoder().decode(
-                DecodableHTTPErrorMessage.self,
-                from: data
-            )
-            throw LoginError.authentication(errorData.message)
-        case 400, 402..<499:
-            throw LoginError.connectivity
-        case success:
-            return try JSONDecoder().decode(DecodableAuthenticationResult.self, from: data).toAuthenticationResult()
-        default:
-            throw LoginError.other
-        }
-    }
-}
-
 struct AuthenticationMapperTests {
 
     @Test func throws_authentication_error_on_401_http_response() async throws {
