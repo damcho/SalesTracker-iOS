840d6870b3753dcb7ce8395b67268e89990fb986
Damian Modernell <dmodernell@cohesionib.com>
2025-02-20 17:15:26 -0300

    Adding extension to URLRequest to be able to read the httpbody

--- SalesTracker/SalesTrackerTests/NetworkingTests/Helpers/URLProtocolStub.swift
+++ SalesTracker/SalesTrackerTests/NetworkingTests/Helpers/URLProtocolStub.swift
@@ -67,3 +67,39 @@ class URLProtocolStub: URLProtocol {
 	
 	override func stopLoading() {}
 }
+
+extension URLRequest {
+
+    func bodySteamAsJSON() -> Any? {
+
+        guard let bodyStream = self.httpBodyStream else { return nil }
+
+        bodyStream.open()
+
+        // Will read 16 chars per iteration. Can use bigger buffer if needed
+        let bufferSize: Int = 16
+
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+
+        var dat = Data()
+
+        while bodyStream.hasBytesAvailable {
+
+            let readDat = bodyStream.read(buffer, maxLength: bufferSize)
+            dat.append(buffer, count: readDat)
+        }
+
+        buffer.deallocate()
+
+        bodyStream.close()
+
+        do {
+            return try JSONSerialization.jsonObject(with: dat, options: JSONSerialization.ReadingOptions.allowFragments)
+        } catch {
+
+            print(error.localizedDescription)
+
+            return nil
+        }
+    }
+}
