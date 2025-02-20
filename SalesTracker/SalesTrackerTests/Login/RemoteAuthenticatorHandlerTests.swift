48af9b2f81044cae0cfc80018f87fd2cc5e9b370
Damian Modernell <dmodernell@cohesionib.com>
2025-02-19 23:37:46 -0300

    test maps_on_http_successful_response

--- SalesTracker/SalesTrackerTests/Login/RemoteAuthenticatorHandlerTests.swift
+++ SalesTracker/SalesTrackerTests/Login/RemoteAuthenticatorHandlerTests.swift
@@ -6,15 +6,53 @@
 //
 
 import Testing
+import Foundation
+
 @testable import SalesTracker
 
+struct EncodableCredentials: Encodable {
+    let username: String
+    let password: String
+}
+
+extension LoginCredentials {
+    func toEncodableCredentials() -> EncodableCredentials {
+        EncodableCredentials(username: username, password: password)
+    }
+}
+
+protocol HTTPClient {
+    func post<T: Encodable>(
+        url: URL,
+        body: T
+    ) async throws -> (httpResponse: HTTPURLResponse, data: Data)
+}
+
+enum HTTPError: Error {
+    case notFound
+}
+
+typealias AuthMapper = (HTTPURLResponse, Data) throws -> AuthenticationResult
+
 struct RemoteAuthenticatorHandler {
-    
+    let httpClient: HTTPClient
+    let url: URL
+    let mapper: AuthMapper
 }
 
 extension RemoteAuthenticatorHandler: Authenticable {
     func authenticate(with credentials: LoginCredentials) async throws -> AuthenticationResult {
-        throw LoginError.connectivity
+        do {
+            let httpResult = try await httpClient.post(
+                url: url,
+                body: credentials.toEncodableCredentials()
+            )
+            return try mapper(
+                httpResult.httpResponse, httpResult.data
+            )
+        } catch is HTTPError {
+            throw LoginError.connectivity
+        }
     }
 }
 
@@ -22,12 +60,49 @@ struct RemoteAuthenticatorHandlerTests {
 
     @Test func throws_connectivity_error_on_http_error() async throws {
         await #expect(throws: LoginError.connectivity, performing: {
-            try await RemoteAuthenticatorHandler().authenticate(with: anyLoginCredentials)
+            let sut = makeSUT(httpStub: .failure(HTTPError.notFound))
+            _ = try await sut.authenticate(with: anyLoginCredentials)
         })
     }
     
     @Test func maps_on_http_successful_response() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        var mapResponseCount = 0
+        let sut = makeSUT(
+            httpStub: .success((successfulHTTPResponse, validToken.data)),
+            mapper: {_, _ in
+                mapResponseCount += 1
+                return anyAuthenticationResult
+            }
+        )
+        
+        _ = try await sut.authenticate(with: anyLoginCredentials)
+        #expect(mapResponseCount == 1)
     }
+}
 
+extension RemoteAuthenticatorHandlerTests {
+    func makeSUT(
+        httpStub: HTTPResult,
+        mapper: @escaping AuthMapper = {_, _ in
+            anyAuthenticationResult
+        }
+    ) -> RemoteAuthenticatorHandler {
+        RemoteAuthenticatorHandler(
+            httpClient: HTTPClientStub(stub: httpStub),
+            url: anyURL,
+            mapper: mapper
+        )
+    }
+}
+
+typealias HTTPResult =  Result<(httpResponse: HTTPURLResponse, data: Data), Error>
+struct HTTPClientStub: HTTPClient {
+    let stub: HTTPResult
+    func post<T>(url: URL, body: T) async throws -> (httpResponse: HTTPURLResponse, data: Data) where T : Encodable {
+        try stub.get()
+    }
+}
+
+var anyURL: URL {
+    URL(string: "https://example.com")!
 }
