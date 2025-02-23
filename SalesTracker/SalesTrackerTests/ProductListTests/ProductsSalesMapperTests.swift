64cf901fda425358d0bbf283b9a226d760ca5f64
Damian Modernell <dmodernell@cohesionib.com>
2025-02-22 22:59:47 -0300

    Implementing product sales mapper test throws_authentication_error_on_401_status_code

---
+++ SalesTracker/SalesTrackerTests/ProductListTests/ProductsSalesMapperTests.swift
@@ -0,0 +1,62 @@
+//
+//  ProductsSalesMapperTests.swift
+//  SalesTrackerTests
+//
+//  Created by Damian Modernell on 22/2/25.
+//
+
+import Testing
+import Foundation
+@testable import SalesTracker
+
+enum ProductsSalesMapper {
+    static let unauthorized = 401
+    static let success = 200
+    
+    static func map(_ response: HTTPURLResponse, _ data: Data) throws -> [Product] {
+        switch response.statusCode {
+        case unauthorized:
+            let errorData = try JSONDecoder().decode(
+                DecodableHTTPErrorMessage.self,
+                from: data
+            )
+            throw LoginError.authentication(errorData.message)
+        case 400, 402..<499:
+            throw HTTPError.notFound
+        case success:
+            return try JSONDecoder().decode(
+                [DecodableProduct].self,
+                from: data
+            ).compactMap({ decodedProduct in
+                decodedProduct.toProduct()
+            })
+        default:
+            throw HTTPError.other
+        }
+    }
+}
+
+struct ProductsSalesMapperTests: MapperSpecs {
+    @Test func throws_authentication_error_on_401_status_code() async throws {
+        #expect(throws: invalidCredentialsAuthError.error, performing: {
+            _ = try ProductsSalesMapper.map(invalidAuthHTTPResponse, invalidCredentialsAuthError.http)
+        })
+    }
+    
+    @Test func throws_connectivity_error_on_not_found_status_code() async throws {
+        
+    }
+    
+    @Test func throws_decoding_error_on_invalid_data() async throws {
+        
+    }
+    
+    @Test func returns_mapped_data_on_successful_200_status_code() async throws {
+        
+    }
+    
+    @Test func throws_other_error_on_other_http_status_code() async throws {
+        
+    }
+
+}
