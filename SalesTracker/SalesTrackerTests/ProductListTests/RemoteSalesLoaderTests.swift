b63ae1561c7e30fb472211cd714669a0ecebce39
Damian Modernell <dmodernell@cohesionib.com>
2025-02-23 16:04:13 -0300

    Implementing RemoteSalesLoader and its tests

---
+++ SalesTracker/SalesTrackerTests/ProductListTests/RemoteSalesLoaderTests.swift
@@ -0,0 +1,59 @@
+//
+//  RemoteSalesLoaderTests.swift
+//  SalesTrackerTests
+//
+//  Created by Damian Modernell on 23/2/25.
+//
+
+import Testing
+@testable import SalesTracker
+import Foundation
+
+typealias RemoteSalesListMapper = ((data: Data, httpResponse: HTTPURLResponse)) throws -> [DecodableSale]
+
+struct RemoteSalesLoader {
+    let httpClient: SalesTrackerHTTPClient
+    let url: URL
+    let mapper: RemoteSalesListMapper
+}
+
+extension RemoteSalesLoader: RemoteSalesLoadable {
+    func loadSales() async throws -> [SalesTracker.DecodableSale] {
+        try await mapper(httpClient.get(from: url))
+    }
+}
+
+struct RemoteSalesLoaderTests {
+
+    @Test func throws_on_products_load_error() async throws {
+        let sut = makeSUT(httpCLientStub: .failure(anyError))
+        
+        await #expect(throws: anyError, performing: {
+            try await sut.loadSales()
+        })
+    }
+    
+    @Test func returns_mapped_products_on_load_success() async throws {
+        let sut = makeSUT(
+            httpCLientStub: .success((productListData.http, successfulHTTPResponse)),
+            mapper: {_ in
+                salesList.decoded
+            }
+        )
+
+        #expect(try await sut.loadSales() == salesList.decoded)
+    }
+}
+
+extension RemoteSalesLoaderTests {
+    func makeSUT(
+        httpCLientStub: HTTPResult,
+        mapper: @escaping RemoteSalesListMapper = {_ in throw anyError }
+    ) -> RemoteSalesLoadable {
+        return RemoteSalesLoader(
+            httpClient: HTTPClientStub(stub: httpCLientStub),
+            url: anyURL,
+            mapper: mapper
+        )
+    }
+}
