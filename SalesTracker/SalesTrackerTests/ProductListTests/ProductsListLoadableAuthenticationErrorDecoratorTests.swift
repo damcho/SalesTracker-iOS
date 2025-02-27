2ca7e154a307c30fdfd1d5ec7f0a4d371e1de011
Damian Modernell <dmodernell@cohesionib.com>
2025-02-27 15:40:49 -0300

    test calls_auth_error_closure_on_auth_error

--- SalesTracker/SalesTrackerTests/ProductListTests/ProductsListLoadableAuthenticationErrorDecoratorTests.swift
+++ SalesTracker/SalesTrackerTests/ProductListTests/ProductsListLoadableAuthenticationErrorDecoratorTests.swift
@@ -8,30 +8,54 @@
 import Testing
 @testable import SalesTracker
 
-struct ProductsListLoadableAuthenticationErrorDecorator {
-    
+struct AuthenticationErrorDecorator {
+    let authErrorHandler: () -> Void
+    let decoratee: ProductSalesLoadable
 }
 
-extension ProductsListLoadableAuthenticationErrorDecorator: ProductSalesLoadable {
+extension AuthenticationErrorDecorator: ProductSalesLoadable {
     func loadProductsAndSales() async throws -> [Product : [Sale]] {
-        throw anyError
+        authErrorHandler()
+        return try await decoratee.loadProductsAndSales()
     }
 }
 
 struct ProductsListLoadableAuthenticationErrorDecoratorTests {
-
+    
     @Test func forwards_error_on_loader_error() async throws {
-        let sut = makeSUT()
+        let sut = makeSUT(stub: .failure(anyError))
         
         await #expect(throws: anyError, performing: {
             try await sut.loadProductsAndSales()
         })
     }
-
+    
+    @Test func calls_auth_error_closure_on_auth_error() async throws {
+        var errorHandlerCallCount = 0
+        let sut = makeSUT(
+            stub: .failure(authError),
+            authErrorHandler: {
+                errorHandlerCallCount += 1
+            }
+        )
+        
+        await #expect(throws: authError, performing: {
+            _ = try await sut.loadProductsAndSales()
+            #expect(errorHandlerCallCount == 1)
+        })
+    }
 }
 
 extension ProductsListLoadableAuthenticationErrorDecoratorTests {
-    func makeSUT() -> ProductsListLoadableAuthenticationErrorDecorator {
-        return ProductsListLoadableAuthenticationErrorDecorator()
+    func makeSUT(
+        stub: Result<[Product : [Sale]], Error>,
+        authErrorHandler: @escaping () -> Void = {}
+    ) -> AuthenticationErrorDecorator {
+        return AuthenticationErrorDecorator(
+            authErrorHandler: authErrorHandler,
+            decoratee: ProductSalesLoadableStub(
+                stub: stub
+            )
+        )
     }
 }
