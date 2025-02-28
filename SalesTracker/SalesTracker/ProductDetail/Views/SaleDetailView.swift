4edbb39de85fd42c255f09d54c5ca29a0e884557
Damian Modernell <dmodernell@cohesionib.com>
2025-02-28 17:44:23 -0300

    Implementing SaleDetailView layout

---
+++ SalesTracker/SalesTracker/ProductDetail/Views/SaleDetailView.swift
@@ -0,0 +1,28 @@
+//
+//  SaleDetailView.swift
+//  SalesTracker
+//
+//  Created by Damian Modernell on 28/2/25.
+//
+
+import SwiftUI
+
+struct SaleDetailView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Hello, World!")
+                Spacer()
+                Text("Hello, World!")
+            }
+            HStack {
+                Text("Hello, World!")
+                Spacer()
+            }
+        }.padding()
+    }
+}
+
+#Preview {
+    SaleDetailView()
+}
