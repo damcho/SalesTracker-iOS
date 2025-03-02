452c6c3288ecf995996ae19fa82f6403736c832f
Damian Modernell <dmodernell@cohesionib.com>
2025-03-02 19:43:42 -0300

    Adding swiftformat to the project

---
+++ SalesTracker/BuildTools/Empty.swift
@@ -0,0 +1,8 @@
+//
+//  Empty.swift
+//  SalesTracker
+//
+//  Created by Damian Modernell on 2/3/25.
+//
+
+import Foundation
---
+++ SalesTracker/BuildTools/Package.swift
@@ -0,0 +1,18 @@
+//
+//  Package.swift
+//  SalesTracker
+//
+//  Created by Damian Modernell on 2/3/25.
+//
+
+// swift-tools-version:6.0
+import PackageDescription
+
+let package = Package(
+    name: "BuildTools",
+    platforms: [.macOS(.v10_13)],
+    dependencies: [
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.55.0")
+    ],
+    targets: [.target(name: "BuildTools", path: "")]
+)
