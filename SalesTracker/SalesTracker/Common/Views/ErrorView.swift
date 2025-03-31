8884820799f511b0520658c8ed933e59480cd987
Damian Modernell <damian.modernell@gmail.com>
2025-03-31 17:18:43 -0300

    Improving ErrorView by returning an empty view if the error is empty

--- SalesTracker/SalesTracker/Common/Views/ErrorView.swift
+++ SalesTracker/SalesTracker/Common/Views/ErrorView.swift
@@ -10,9 +10,13 @@ import SwiftUI
 struct ErrorView: View {
     @StateObject var viewModel: ErrorViewModel
     var body: some View {
-        Text(viewModel.errorMessage)
-            .secondaryListText()
-            .foregroundStyle(.red)
+        if viewModel.errorMessage.isEmpty {
+            EmptyView()
+        } else {
+            Text(viewModel.errorMessage)
+                .secondaryListText()
+                .foregroundStyle(.red)
+        }
     }
 }
 
